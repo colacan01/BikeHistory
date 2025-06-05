79f16e8f23c77da18a52821cabe06d4c09858b5a
JinHo, Ko <colacan01@hotmail.com>
2025-06-05 13:50:24 +0900

    Revert "RoleGuard 인증 방식 변경 및 사용자 역할 추가"
    This reverts commit b925015ae9e46bcc1442661da17f7f0f06c4cd47.

--- bikehistory.client/src/app/guards/role.guard.ts
+++ bikehistory.client/src/app/guards/role.guard.ts
@@ -1,27 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Router, ActivatedRouteSnapshot } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RoleGuard {
+export class RoleGuard implements CanActivate {
   
   constructor(private authService: AuthService, private router: Router) {}
   
   canActivate(route: ActivatedRouteSnapshot): boolean {
     const requiredRoles = route.data['roles'] as Array<string>;
     
-    //if (!this.authService.isAuthenticated()) {
-    //  this.router.navigate(['/login']);
-    //  return false;
-    //}
-
-    if (!this.authService.isLoggedIn) {
+    if (!this.authService.isAuthenticated()) {
       this.router.navigate(['/login']);
       return false;
     }
-
+    
     const userRoles = this.authService.getUserRoles();
     const hasRequiredRole = requiredRoles.some(role => userRoles.includes(role));
     
